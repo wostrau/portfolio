130ad052a32792af5fc1a34d2e29cda21853e21f
wostrau <a.ostrovtsov@gmail.com>
2022-12-08 23:29:12 +0100

    IN PROGRESS - 4th lesson of layout playlist / stopped at 54:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 //import './App.css';
 import Header from './header/Header';
-import Main from './main/Main';
+import {Main} from './main/Main';
 import Skills from './skills/Skills';
 import Projects from './projects/Projects';
 import Hire from './hire/Hire';
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import style from './Main.module.scss';
-import styleContainer from '../common/styles/Container.module.css'
+import Particles from 'react-particles-js';
+import Fade from 'react-reveal/Fade';
 
-const Main = () => {
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 80,
+            destiny: {
+                enable: true,
+                value_area: 800
+            }
+        }
+    }
+};
+
+export const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
-                <div className={style.greeting}>
-                    <span>Hi there!</span>
-                    <h1>I am<br/>Aliaksandr Astrautsou</h1>
-                    <p>frontend developer</p>
+            <Particles
+                className={style.particles}
+                params={particlesOptions}
+            />
+            <Fade top>
+                <div className={style.container}>
+                    <div className={style.greeting}>
+                        <span>Hi there!</span>
+                        <span>I am Aliaksandr<span>Astrautsou</span></span>
+                        <h1>frontend developer</h1>
+                    </div>
+                    <div className={style.picture}>
+                        <div className={style.image}></div>
+                    </div>
                 </div>
-                <div className={style.picture}></div>
-            </div>
+            </Fade>
         </div>
     );
 };
-
-export default Main;
